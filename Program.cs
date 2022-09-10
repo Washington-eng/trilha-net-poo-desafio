233efbbd40921891b7bc61306ea752b707618614
washingtoncosta1 <washington.ferreira1@hotmail.com>
2022-09-10 01:12:13 -0300

    ## solution to trilla-net-poo-desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Nokia - Teste:");
+Nokia nokia = new Nokia("12345678", "Modelo X", "12345678910", 64);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("App Teste 1");
+
+
+System.Console.WriteLine("Smartphone Iphone - Teste:");
+Iphone iphone = new Iphone("98765432", "Modelo Y", "12345678910", 128);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("App Teste 2");
